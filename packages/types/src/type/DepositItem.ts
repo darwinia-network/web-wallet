b4ab3eaf89bc2a7cda47ad71a5c5aca0499bda77
WoeOm <1015996366@qq.com>
2019-09-12 17:48:38 +0800

    add staking.ledger type

---
+++ packages/types/src/type/DepositItem.ts
@@ -0,0 +1,40 @@
+// Copyright 2017-2019 @polkadot/types authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import Compact from '../codec/Compact';
+import Struct from '../codec/Struct';
+import Balance from './Balance';
+import BlockNumber from './BlockNumber';
+import Moment from '../primitive/Moment';
+
+/**
+ * @name DepositItem
+ * @description
+ * Just a Balance/BlockNumber tuple to encode when a chunk of funds will be unlocked
+ */
+export default class DepositItem extends Struct {
+  constructor (value?: any) {
+    super({
+      value: Compact.with(Balance),
+      start_time: Compact.with(Moment),
+      expire_time: Compact.with(Moment),
+    }, value);
+  }
+
+  /**
+   * @description Amount of funds to be unlocked
+   */
+  get value (): Balance {
+    return (this.get('value') as Compact).toBn() as Balance;
+  }
+
+  get start_time (): Moment {
+    return (this.get('start_time') as Compact).unwrap() as Moment;
+  }
+
+  get expire_time (): Moment {
+    return (this.get('expire_time') as Compact).unwrap() as Moment;
+  }
+
+}
---
+++ packages/types/src/type/Unlocking.ts
@@ -0,0 +1,44 @@
+// Copyright 2017-2019 @polkadot/types authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import Compact from '../codec/Compact';
+import Struct from '../codec/Struct';
+import Balance from './Balance';
+import BlockNumber from './BlockNumber';
+import U32 from '../primitive/U32'
+import UnlockingValue from './UnlockingValue'
+import Bool from '../primitive/Bool';
+
+/**
+ * @name Unlocking
+ * @description
+ * Just a Balance/BlockNumber tuple to encode when a chunk of funds will be unlocked
+ */
+export default class Unlocking extends Struct {
+  constructor (value?: any) {
+    super({
+      value: UnlockingValue,
+      era: Compact.with(U32),
+      is_time_deposit: Bool
+    }, value);
+  }
+
+  /**
+   * @description Era number at which point it'll be unlocked
+   */
+  get era (): U32 {
+    return (this.get('era') as Compact).toBn() as U32;
+  }
+
+  /**
+   * @description Amount of funds to be unlocked
+   */
+  get value (): UnlockingValue {
+    return this.get('value') as UnlockingValue;
+  }
+
+  get is_time_deposit(): Bool{
+    return this.get('is_time_deposit') as Bool;
+  }
+}
